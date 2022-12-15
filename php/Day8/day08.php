6b0dcb4026e2cc42d5b644e820996fb736969c6b
Matt <eclecticappdevelopment@gmail.com>
2022-12-15 10:47:05 +0000

    Solution added

--- php/Day8/day08.php
+++ php/Day8/day08.php
@@ -32,20 +32,9 @@ if ($handle) {
 $totalVisible = 99 + 99 + 97 + 97;
 //STORE THE ROW SIZE
 $size = $i;
-echo 'Forest Length: ' . $size . '<br>';
-//TRANSPOSE THE ARRAY (https://stackoverflow.com/a/3423692/16384571)
-$transposed = array_map(null, ...$forest);
-
-$visibleForest = array();
-$visibleForest[0] = array_fill(0,99,1);
-$visibleForest[98] = array_fill(0,99,1);
 
 //ITERATE ROWS FROM 1 - (MAX - 1) - IGNORE THE TOP AND BOTTOM ROW
 for($row = 1; $row < (count($forest) - 1); $row++){
-
-	$visibleForest[$row] = array();
-	$visibleForest[$row][0] = 1;
-	$visibleForest[$row][98] = 1;
 
 	//ITERATE COLS FROM 1 TO (MAX - 1) - IGNORE THE LEFT AND RIGHT COLS
 	for($col = 1; $col < ($size - 1); $col++){
@@ -60,19 +49,6 @@ for($row = 1; $row < (count($forest) - 1); $row++){
 		$upMax = $row > 1 ? max(array_slice($theCol, 0, $row,true)) : $forest[0][$col];
 		$downMax = $row < 97 ? max(array_slice($theCol, $row + 1,99,true)) : $forest[98][$col];
 
-		echo "<h1>CHECK [$row][$col] - HEIGHT: " . $thisHeight . '</h1>';
-		echo '<ul>';
-		echo '<li>LEFT MAX: ' . $leftMax;
-			echo ($leftMax < $thisHeight) ? ' - VISIBLE!</li>' : '</li>';
-		echo '<li>RIGHT MAX: ' . $rightMax;
-			echo ($rightMax < $thisHeight) ? ' - VISIBLE!</li>' : '</li>';
-		echo '<li>UP MAX: ' . $upMax;
-			echo ($upMax < $thisHeight) ? ' - VISIBLE!</li>' : '</li>';
-		echo '<li>DOWN MAX: ' . $downMax;
-			echo ($downMax < $thisHeight) ? ' - VISIBLE!</li>' : '</li>';
-		echo '</ul>';
-
-
 		//IF ANY OF THE HEIGHTS ARE ABOVE THE MAX FOR THE LEFT/RIGHT/UP/DOWN
 		if(
 			//CHECK LEFT - GET THE MAX VALUE IN THIS ROW FROM 0 TO (COL - 1)
@@ -85,181 +61,8 @@ for($row = 1; $row < (count($forest) - 1); $row++){
 			($downMax < $thisHeight)
 		){
 			$totalVisible++;
-			$visibleForest[$row][$col] = 1;
-			echo '<h3>Tree at [' . $row . ', ' . $col . '] IS VISIBLE (total: ' . $totalVisible . ')</h3><hr>'; 
-		}else{
-			$visibleForest[$row][$col] = 0;
-			echo '<h3>Tree at [' . $row . ', ' . $col . '] IS NOT VISIBLE</h3><hr>'; 
 		}
-
-		/*
-		if(
-			($maxLeft < $thisHeight) or
-			($maxRight < $thisHeight) or
-			($maxUp < $thisHeight) or
-			($maxDown < $thisHeight)
-		){
-			$totalVisible++;
-			echo '<h3>Tree at [' . $row . ', ' . $col . '] IS VISIBLE (total: ' . $totalVisible . ')</h3><hr>'; 
-		}else{
-			echo '<h3>Tree at [' . $row . ', ' . $col . '] IS NOT VISIBLE</h3><hr>'; 
-		}*/
-
-		/*
-		//PASS TO FUNCTION TO CHECK IF VISIBLE IN AT LEAST 1 DIRECTION
-		if(checkVisible($forest, $row, $col) === true){
-
-			//DEBUG
-			echo 'Tree at [' . $row . ', ' . $col . '] IS VISIBLE<br>'; 
-			//INCREMENT THE TOTAL VISIBLE TREE COUNT
-			$totalVisible++;
-			//break 1;
-		}else{
-
-			//DEBUG
-			echo 'Tree at [' . $row . ', ' . $col . '] IS NOT VISIBLE<br>'; 
-		}
-		*/
 	}
 }
 
-//echo '<hr>';
 echo '<h1>Total Visible: ' . $totalVisible . '</h1>';
-
-echo '<pre>';
-$spacer = "\t";
-$spacer = "&nbsp;";
-$spacer = "";
-echo '<script>
-
-//HIGHLIGHT PROBLEM CELL
-highlight(10,39,6);
-
-function highlight(row, col, value){
-	console.log("HIGHLIGHT",row,col);
-	//RESET STYLE
-	for(i = 0; i < 99; i++){
-		cells = document.querySelectorAll(".row" + i);
-		cells.forEach(cell => {
-			cell.style.backgroundColor = "white";
-		});
-		cells = document.querySelectorAll(".col" + i);
-		cells.forEach(cell => {
-			cell.style.backgroundColor = "white";
-		});
-	}
-	//APPLY STYLE
-	cells = document.querySelectorAll(".row" + row);
-	cells.forEach(cell => {
-		if(parseInt(cell.innerHTML) >= value){
-			cell.style.backgroundColor = "red";
-		}else{
-			cell.style.backgroundColor = "yellow";
-		}
-	});
-	cells = document.querySelectorAll(".col" + col);
-	cells.forEach(cell => {
-		if(parseInt(cell.innerHTML) >= value){
-			cell.style.backgroundColor = "red";
-		}else{
-			cell.style.backgroundColor = "yellow";
-		}
-	});
-}
-</script>';
-
-for($row = 0; $row < $size; $row++){
-	for($col = 0; $col < $size; $col++){
-		echo '<span class="row' . $row . ' col' . $col . '">' . $forest[$row][$col] . '</span>' . $spacer;
-	}
-	//echo "\t\t\t";
-	echo "&nbsp";
-	for($col = 0; $col < $size; $col++){
-		echo '<span onclick="highlight(' . $row . ', ' . $col . ', ' . $forest[$row][$col] . ')" class="row' . $row . ' col' . $col . '">' . $visibleForest[$row][$col] . '</span>' . $spacer;
-	}
-	echo "\n";
-}
-//print_r($forest);
-//echo '<hr>';
-//print_r($transposed);
-//print_r($visibleForest);
-echo '</pre>';
-
-for($row = 0; $row < 99; $row++){
-	for($col = 0; $col < 99; $col++){
-		if($forest[$row][$col] !== $transposed[$col][$row]){
-			echo "MISMATCH AT [$row][$col]<br>";
-		}
-	}
-}
-
-
-function checkVisible($forest, $row, $col){
-
-	//THE HEIGHT OF THE CURRENT TREE
-	$height = $forest[$row][$col];
-	//THE LAST COL
-	$maxCol = count($forest[$row]);
-	
-	$visible = true;
-	//CHECK LEFT VISIBLE
-	for($i = 0; $i < $col; $i++){
-		if($forest[$row][$i] >= $height){
-			echo "[$row][$col] IS NOT LEFT VISIBLE DUE TO COL $i WITH HEIGHT " . $forest[$row][$i] . "<br>";
-			$visible = false;
-			break;
-		}
-	}
-	if($visible){
-		echo "[$row][$col] IS LEFT VISIBLE<br>";
-		return true;
-	}
-	//if($visible) return $visible;
-
-	$visible = true;
-	//CHECK RIGHT VISIBLE
-	for($i = ($col + 1); $i < $maxCol; $i++){
-		if($forest[$row][$i] >= $height){
-			echo "[$row][$col] IS NOT RIGHT VISIBLE DUE TO COL $i WITH HEIGHT " . $forest[$row][$i] . "<br>";
-			$visible = false;
-			break;
-		}
-	}
-	if($visible){
-		echo "[$row][$col] IS RIGHT VISIBLE<br>";
-		return true;
-	}
-	//if($visible) return $visible;
-
-	$visible = true;
-	//CHECK UP VISIBLE
-	for($i = 0; $i < $col; $i++){
-		if($forest[$i][$col] >= $height){
-			echo "[$row][$col] IS NOT UP VISIBLE DUE TO ROW $i WITH HEIGHT " . $forest[$i][$col] . "<br>";
-			$visible = false;
-			break;
-		}
-	}
-	if($visible){
-		echo "[$row][$col] IS UP VISIBLE<br>";
-		return true;
-	}
-	//if($visible) return $visible;
-
-	$visible = true;
-	//CHECK DOWN VISIBLE
-	for($i = ($col + 1); $i < $maxCol; $i++){
-		if($forest[$i][$col] >= $height){
-			echo "[$row][$col] IS NOT DOWN VISIBLE DUE TO ROW $i WITH HEIGHT " . $forest[$i][$col] . "<br>";
-			$visible = false;
-			break;
-		}
-	}
-	if($visible){
-		echo "[$row][$col] IS DOWN VISIBLE<br>";
-		return true;
-	}
-	//if($visible) return $visible;
-
-	return false;
-}
