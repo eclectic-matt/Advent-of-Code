3d55543d49fe4d917485fcf95483ddad90ce53b6
Matt <eclecticappdevelopment@gmail.com>
2022-12-21 10:19:40 +0000

    Clean solution

--- php/Day11/day11-pt2.php
+++ php/Day11/day11-pt2.php
@@ -1,6 +1,6 @@
 <?php
 
-//ADVENT OF CODE 2022 - DAY 11
+//ADVENT OF CODE 2022 - DAY 11, PART 2
 
 //OPEN FILE HANDLE (READ)
 $handle = fopen("input.txt", "r");
@@ -11,13 +11,9 @@ $monkeys = array();
 
 //INIT ROUND
 $round = 0;
-//
+//THE NUMBER OF ROUNDS TO PROCESS
 $roundLimit = 10000;
-$roundBreak = 1000;
-$countItems = 0;
-//DEBUG
-$startTime = microtime(true);
-$initTime = microtime(true);
+
 
 if ($handle) {
 	//READ FILE LINE BY LINE
@@ -37,9 +33,7 @@ if ($handle) {
 			//PROCESS STARTING ITEMS
 			$items = trim(substr($line, 18));
 			$items = explode(", ",$items);
-			//echo 'MONKEY ' . $monkeyId . ' HAS ITEMS: "' . implode(",",$items) . '"<br>';
 			$monkeys[$monkeyId]['items'] = $items;
-			$countItems += count($items);
 		
 		}else if(substr($line,2,9) === 'Operation'){
 
@@ -50,7 +44,6 @@ if ($handle) {
 			$operand = $opLine[1];
 			//THE OPERATION FACTOR (1, 2, old, 7)
 			$opFactor = trim($opLine[2]);
-			//echo 'OPERATION - OP=' . $operand . ', FACT=' . $opFactor . '<br>';
 			$monkeys[$monkeyId]['opFactor'] = $opFactor;
 			$monkeys[$monkeyId]['operand'] = $operand;
 		
@@ -80,15 +73,12 @@ if ($handle) {
 			$falseThrow = trim(end($explodeTest));
 			//echo 'IF FALSE, THROW TO ' . $falseThrow . '<br>';
 			$monkeys[$monkeyId]['falseThrow'] = $falseThrow;
-
 		}
 	}
 }
 
-
+//STORE THE COUNT OF MONKEYS
 $monkeyCount = $monkeyId + 1;
-echo 'MONKEY COUNT : ' . $monkeyCount . '<br>';
-
 
 //RIGHT - THIS IS THE "TRICK" THAT ALLOWS THIS TO WORK
 //https://en.wikipedia.org/wiki/Chinese_remainder_theorem
@@ -98,14 +88,10 @@ echo 'MONKEY COUNT : ' . $monkeyCount . '<br>';
 //WHERE THE $superModulo IS THE PRODUCT OF ALL THE DIVISORS
 //IT'S AN ANNOYING HACK WHICH CAUGHT ME OUT FOR A WHILE!
 $superModulo = array_reduce($monkeys, function($prev, $current){
-	echo 'MULTIPLY BY ' . $current['divisor'] . '<br>';
+
 	return $current['divisor'] * $prev;
 }, 1);
 
-echo 'SUPER MODULO: ' . $superModulo . '<br>';
-
-//OUTPUT TOTAL ITEMS
-echo '<h1>TOTAL INITIAL ITEMS = ' . $countItems . '</h1>';
 
 //WHILE WE ARE STILL PROCESSING 
 while ($round < $roundLimit){
@@ -148,7 +134,6 @@ while ($round < $roundLimit){
 			//RUN THE TEST USING THE DIVISOR METHOD
 			$result = (gmp_div_r($newItem, $monkeys[$monkeyId]['divisor'], GMP_ROUND_ZERO) == 0);
 			
-
 			if($result){
 
 				//GET THE "TRUE" MONKEY
@@ -173,42 +158,13 @@ while ($round < $roundLimit){
 
 	//INCREMENT ROUND
 	$round++;
-
-	if(($round % $roundBreak) == 0){
-		echo '<h2>AFTER ROUND ' . $round . '</h2>';
-		echo '<em>Time Taken: ' . (microtime(true) - $startTime) . 's</em><br>';
-		//RESET START TIME
-		$startTime = microtime(true); 
-		foreach($monkeys as $id => $monkey){
-			echo 'Monkey ' . $id . ' inspected items ' . $monkey['inspects'] . ' times<br>'; 
-		}
-	}
-}
-
-//OUTPUT FINALE
-echo '<h2>AFTER ROUND ' . $round . '</h2>';	
-
-//OUTPUT TIME
-echo '<em>Time Taken: ' . (microtime(true) - $startTime) . 's</em><br>';
-//RESET START TIME
-$startTime = microtime(true);
-
-foreach($monkeys as $id => $monkey){
-	echo 'Monkey ' . $id . ' inspected items ' . $monkey['inspects'] . ' times<br>'; 
 }
 
 $inspects = array();
-$countItems = 0;
 foreach($monkeys as $id => $monkey){
-	//echo 'MONKEY ' . $id . ' INSPECTED ' . $monkey['inspects'] . ' ITEMS AND ENDED WITH ' . count($monkey['items']) . '<br>';
-	$countItems += count($monkey['items']);
 	$inspects[] = $monkey['inspects'];
 }
 rsort($inspects);
 
 //OUTPUT
-echo '<h1>TOTAL END ITEMS = ' . $countItems . '</h1>';
-//echo '<h1>MULTIPLY TOP 2 = ' . ($inspects[0] * $inspects[1]) . '</h1>';
-echo '<h1>TOP 2 = ' . $inspects[0] . ' AND ' . $inspects[1] . '</h1>';
 echo '<h1>MULTIPLY TOP 2 = ' . gmp_mul($inspects[0], $inspects[1]) . '</h1>';
-echo 'TOTAL TIME TAKEN: ' . (microtime(true) - $initTime) . 's<br>';
